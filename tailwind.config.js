1e6c7231424b3809893edd38fd69fe8328989926
satya <128298191+satyasudharsan@users.noreply.github.com>
2023-09-29 22:42:08 +0800

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container: { 
+      center: true,
+    padding: '16px',
+  },
+    extend: {
+      colors : {
+        primary: '#0ea5e9',
+        dark: '#020617'
+      },
+      screens: {
+        '2xl': '1320px',
+      },
+    },
+  },
+  plugins: [],
+};
+
